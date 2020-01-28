deb60cf592103c24a13f890e4ac45addc824e40e
mikk357 <mikk357@yandex.com>
2020-01-28 15:29:28 +0300

    just Uwu

--- Program.cs
+++ Program.cs
@@ -4,9 +4,33 @@ namespace uwu
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string[] lines = new string[7] {
+            "UwU     UwU                   UwU     UwU",
+            "UwU     UwU                   UwU     UwU",
+            "UwU     UwU                   UwU     UwU",
+            "UwU     UwU  UwU         UwU  UwU     UwU",
+            "UwU     UwU   UwU  UwU  UwU   UwU     UwU",
+            " UwUwUwUwU     UwUwUwUwUwU     UwUwUwUwU ",
+            "  UwUwUwU       UwU   UwU       UwUwUwU  "
+        };
+        static void Main()
         {
-            Console.WriteLine("UwU World!");
+            Console.Clear();
+            Console.Write("\n");
+            foreach (string line in lines)
+            {
+                Console.Write(centeredString(line, Console.WindowWidth));
+            }
+        }
+        private static string centeredString(string s, int width)
+        {
+            if (s.Length >= width)
+                return s;
+
+            int leftPadding = (width - s.Length) / 2;
+            int rightPadding = width - s.Length - leftPadding;
+
+            return (new string(' ', leftPadding) + s + new string(' ', rightPadding));
         }
     }
 }
